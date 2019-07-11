6ccc3898aa5eb7acb5be2d0a839f3956493c08c9
candraw21 <52759859+candraw21@users.noreply.github.com>
2019-07-11 13:39:45 -0400

    Add files via upload

---
+++ bt21.js
@@ -0,0 +1,8 @@
+<script language="javascript">
+        function MouseRollover(MyImage) {
+        MyImage.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSix2AqAku2rBqFn2thAbodBdHjwudQztqSFULxnVHOiFurcr7B";
+    }
+        function MouseOut(MyImage) {
+        MyImage.src = "https://media1.tenor.com/images/4297242878e53ebd3d166a3db53a9eda/tenor.gif?itemid=11769999";
+    }
+</script>
